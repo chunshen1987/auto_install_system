a43b936579a99768605363f460eba15a5a75abad
Chun Shen <chunshen@zeos185.physics.wayne.edu>
2019-12-09 14:58:41 -0500

    add powerline package to vim

--- mac/install_vim_packages.sh
+++ mac/install_vim_packages.sh
@@ -5,4 +5,5 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle && \
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone --depth=1 https://github.com/Lokaltog/vim-powerline.git
 

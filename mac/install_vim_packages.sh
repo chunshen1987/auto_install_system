6ce07149483cc7c3f17b2706ab5ca6ad176a3e1a
Chun Shen <chunshen@zeos185.physics.wayne.edu>
2019-12-09 14:36:42 -0500

    add vim packages

--- mac/install_vim_packages.sh
+++ mac/install_vim_packages.sh
@@ -3,3 +3,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+cd ~/.vim/bundle && \
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+
